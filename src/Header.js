044da3a898840d4ed50bd6118f0bca0048d9f542
Eswar Reddy <eswarreddy7999@gmail.com>
2022-09-22 13:45:57 +0530

    version 1.0

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <>
+        <div className='header'>
+            <div>
+                <h1>Website</h1>
+            </div>
+            <div>
+                <a href='#'>Home</a>
+                <a href='#'>Home</a>
+                <a href='#'>Home</a>
+                <a href='#'>Home</a>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Header
